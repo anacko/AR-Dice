8ecef8c02be3d5d3adb26907d4d07148791b4070
Ana Carolina K <anacko@gmail.com>
2022-01-10 18:54:08 -0500

    more setup

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # AR-Dice
 A simple page to show dice with AR
+
+# Source
+[3d model](https://www.cgtrader.com/free-3d-models/interior/living-room/free-dice)
--- app.js
+++ app.js
@@ -2,8 +2,15 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('<a href="./main">Click to go to main.</a>')
+})
+
+app.get('/main', (req, res) => {
+  res.render('main.ejs')
 })
 
 app.listen(port, () => {
