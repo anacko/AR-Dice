4ef36968c13062b9a44987f9f4c6f825482ce916
Ana Carolina K <anacko@gmail.com>
2022-01-18 04:30:51 -0500

    Adjust js files, post routes and presentation

--- app.js
+++ app.js
@@ -30,16 +30,17 @@ app.post('/', (req, res) => {
   const templateVars = { d6Value: req.cookies['d6-value'] }
   res.render('index.ejs', templateVars)
 })
-// app.post('/new_d6', (req, res) => {
-//   const newD6 = Math.floor(Math.random()*6 + 1)
-//   res.cookie('d6-value', newD6)
-//   res.redirect('/')
-// })
 
-// app.post('/reset_d6', (req, res) => {
-//   res.cookie('d6-value', '0')
-//   res.redirect('/')
-// })
+app.post('/new_d6', (req, res) => {
+  const newD6 = Math.floor(Math.random()*6 + 1)
+  res.cookie('d6-value', newD6)
+  res.redirect('/')
+})
+
+app.post('/reset_d6', (req, res) => {
+  res.cookie('d6-value', '0')
+  res.redirect('/')
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
